3af0be3f63b1964f7cd96dab5c330269b8f3b38a
ibrahim <ibrahimmashaqi04@gmail.com>
2026-01-15 23:58:04 +0200

    2.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <li>
-      {part.name} {part.exercises}
-    </li>
-  );
-};
-
-const Content = ({ parts }) => {
-  console.log(parts);
-  const total = parts.reduce((sum, part) => (sum += part.exercises), 0);
-  return (
-    <div>
-      <ul>
-        {parts.map((part) => (
-          <Part key={part.id} part={part} />
-        ))}
-      </ul>
-      <h3>Total of {total} exercises</h3>
-    </div>
-  );
-};
-const Course = ({ course }) => {
-  const { name, parts } = course;
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
@@ -81,6 +47,7 @@ const App = () => {
   ];
   return (
     <div>
+      <h1>Web development curriculum</h1>
       {courses.map((course) => {
         return <Course key={course.id} course={course} />;
       })}
